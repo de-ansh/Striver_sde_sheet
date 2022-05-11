c83804cad45ff5b7ef08574f1188a323567ee3a0
Ashish Mishra <anshm9448@gmail.com>
2022-05-11 19:28:20 +0530

    nextperm solved with better approach

--- Arrays/nextpermu.cpp
+++ Arrays/nextpermu.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+/*#include<bits/stdc++.h>
 using namespace std;
 
 int main() {
@@ -9,4 +9,70 @@ int main() {
     cout<<arr[0]<<" "<<arr[1]<<" "<<arr[2];
     
     return 0;
-}
+}*/
+#include<bits/stdc++.h>
+
+using namespace std;
+/*class Solution {
+  private:
+    void recurPermute(vector < int > & ds, vector < int > & nums, vector < vector < int >> & ans, int freq[]) {
+      if (ds.size() == nums.size()) {
+        ans.push_back(ds);
+        return;
+      }
+      for (int i = 0; i < nums.size(); i++) {
+        if (!freq[i]) {
+          ds.push_back(nums[i]);
+          freq[i] = 1;
+          recurPermute(ds, nums, ans, freq);
+          freq[i] = 0;
+          ds.pop_back();
+        }
+      }
+    }
+  public:
+    vector < vector < int >> permute(vector < int > & nums) {
+      vector < vector < int >> ans;
+      vector < int > ds;
+      int freq[nums.size()];
+      for (int i = 0; i < nums.size(); i++) freq[i] = 0;
+      recurPermute(ds, nums, ans, freq);
+      return ans;
+    }
+};
+
+int main() {
+  Solution obj;
+  vector<int> v{1,3,2};
+  vector < vector < int >> sum = obj.permute(v);
+  for(int i=0;i<sum.size();i++){
+      if(sum[i]==v)
+        for (int j = 0; j < sum[i+1].size(); j++)
+             cout << sum[i+1][j] << " ";
+  }
+  
+    
+    cout << endl;
+}*/
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size(), k, l;
+    	for (k = n - 2; k >= 0; k--) {
+            if (nums[k] < nums[k + 1]) {
+                break;
+            }
+        }
+    	if (k < 0) {
+    	    reverse(nums.begin(), nums.end());
+    	} else {
+    	    for (l = n - 1; l > k; l--) {
+                if (nums[l] > nums[k]) {
+                    break;
+                }
+            } 
+    	    swap(nums[k], nums[l]);
+    	    reverse(nums.begin() + k + 1, nums.end());
+        }
+    }
+};
